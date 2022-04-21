da40262b269f9b36cc259f55fdbb8c43af327524
WalidRahman35 <82604433+WalidRahman35@users.noreply.github.com>
2022-04-21 22:14:52 +0600

    Add files via upload

---
+++ database/migrations/2020_10_08_062642_create_user_models_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_models', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('usernumber');
+            $table->string('useramount')->default('5000');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_models');
+    }
+}
